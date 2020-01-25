be7f52599a2840f5f19eb873c814558c693442d2
gabaid <daniel.gabai@hotmail.fr>
2020-01-25 17:34:16 +0100

    skeleton is the good one, essai2 is not good

--- Makefile
+++ Makefile
@@ -8,7 +8,7 @@ TARGET   := program
 LIBS     :=`pkg-config --cflags --libs opencv`
 INCLUDE  := -Iinclude/
 SRC      :=$(wildcard src/*.cpp)   \
-   $(wildcard src/skeleton_essai/*.cpp) \
+   $(wildcard src/skeleton/*.cpp) \
 
 OBJECTS := $(SRC:%.cpp=$(OBJ_DIR)/%.o)
 
--- src/skeleton/test_skeleton.cpp
+++ src/skeleton/test_skeleton.cpp
@@ -1,3 +1,6 @@
+#include "get_skeleton.hpp"
+#include "normalize.hpp"
+
 #include <opencv2/core.hpp>
 #include <opencv2/imgproc.hpp>
 #include <opencv2/imgcodecs.hpp>
@@ -9,35 +12,23 @@ using namespace cv;
 using namespace std;
 
 
+
 int main()
 {
-  Mat img;
-  img = imread( "images/clean_finger.png", IMREAD_GRAYSCALE );
-  imshow( "remap", img );
+  Mat src;
+  src = imread( "images/clean_finger.png", IMREAD_GRAYSCALE );
+  cout << "the mean is: " << mean_grey(src) << endl;
+  cout << "the var is: " << var_grey(src) << endl;
+  //src = normalize(src, 127, 5000);
+  //GaussianBlur(src, src, Size(3,3), 0);
+  imshow( "YOUPI", src );
   waitKey(0);
-  threshold(img, img, 127, 255, cv::THRESH_BINARY);
-  Mat skel(img.size(), CV_8UC1, cv::Scalar(0));
-  Mat temp;
-  Mat eroded;
-  imshow( "remap", img );
+  threshold(src, src, 127, 255, THRESH_BINARY);
+  imshow( "YOUPI", src );
   waitKey(0);
-  Mat element = getStructuringElement(MORPH_CROSS, Size(3, 3));
-
-  bool done;
-  do
-  {
-    erode(img, eroded, element);
-    dilate(eroded, temp, element); // temp = open(img)
-    subtract(img, temp, temp);
-    bitwise_or(skel, temp, skel);
-    eroded.copyTo(img);
-
-    done = (countNonZero(img) == 0);
-  } while (!done);
-  Mat element2 = getStructuringElement(MORPH_OPEN, Size(3, 3));
-  erode(skel, skel, element2);
-  dilate(skel, skel, element2);
-  imshow( "remap", skel );
+  src = get_skeleton(src);
+  imshow( "YOUPI", src );
   waitKey(0);
+  //imwrite("warp1_skel1.png", src);
   return 0;
 }
