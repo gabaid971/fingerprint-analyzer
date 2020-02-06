ae04fa28fef72a9e9f99b894ac2c41c380fe0f9c
Thu Thao Huynh <thu-thao.huynh@grenoble-inp.org>
2020-02-06 17:00:33 +0100

    Update starter3a.cpp

--- filter_convolution.cpp
+++ filter_convolution.cpp
@@ -4,20 +4,28 @@
 #include <opencv2/highgui.hpp>
 #include <opencv2/core/hal/interface.h>
 #include <iostream>
-#include <algorithm>
 
 using namespace cv;
 using namespace std;
 
+/*
+fn flip(Mat& kernel)
+param kernel: kernel matrix
+param temp: the extra matrix to save matrix during flipping matrix
+return flipped matrix
+*/
+
 void flip(Mat& kernel)
 {
 	Mat temp;
+	//flip matrix from bottom to top
 	temp.create( kernel.size(), kernel.type());
 	for (int i = 0; i < kernel.rows; i++)
 		for (int j = 0;j < kernel.cols; j++)
 			temp.at<float>(kernel.rows-i - 1,j) = kernel.at<float>(i,j);
 
 	kernel = temp;
+	//flip matrix from left to right
 	for (int i = 0; i < kernel.cols; i++)
 		for (int j = 0; j < kernel.rows; j++)
 			temp.at<float>(j,kernel.cols-i-1) = kernel.at<float>(j,i);
@@ -25,11 +33,29 @@ void flip(Mat& kernel)
 	kernel = temp;
 }
 
+/*
+fn padding(Mat &image, Mat kernel)
+param kernel: kernel matrix
+param image: original image matrix
+param p: the number of row/ column which needs to pad
+return matrix with padding zeros border
+*/
+
 void padding(Mat &image, Mat kernel){
   int p = (kernel.rows - 1)/2;
   copyMakeBorder(image, image, p, p, p, p, 0);
-	cout<<image.size();
 }
+
+/*
+fn convolution_2D(Mat image, Mat kernel)
+param kernel: kernel matrix
+param image: original image matrix
+param r_i: the number of rows of original image before changing anything
+param c_i: the number of cols of original image before changing anything
+param O: output matrix (convolution matrix)
+param sum: sum of product between kernel matrix and original image (it will be reset when kernel matrix is shifted )
+return matrix after convolution
+*/
 
 void convolution_2D(Mat image, Mat kernel)
 {
@@ -38,7 +64,6 @@ void convolution_2D(Mat image, Mat kernel)
 
   Mat O;
   O.create(r_i,c_i, image.type());
-	cout<<O.size();
 
   for (int i = 0; i < r_i; i++){
     for (int j = 0; j < c_i; j++)
@@ -53,17 +78,16 @@ void convolution_2D(Mat image, Mat kernel)
 			O.at<float>(i,j) = sum;
     }
   }
-	O.convertTo(O, CV_32FC1);
+	normalize(O,O,255,0,NORM_MINMAX,CV_32SC1);
 
-	namedWindow( "convolution", CV_WINDOW_AUTOSIZE );
-	imshow( "convolution", O);
+	imwrite("convolution.png",O); //save image
 }
 
 int main()
 {
-    Mat image,kernel, input;
+    Mat image,kernel;
 		int kernel_size = 3;
-		kernel = Mat::zeros(kernel_size, kernel_size, CV_32F);//(float)(kernel_size*kernel_size);
+		kernel = Mat::zeros(kernel_size, kernel_size, CV_32F);
 		kernel.at<float>(1,1) = 1;
 
     image = imread( "clean_finger.png", IMREAD_GRAYSCALE); // Read the file
